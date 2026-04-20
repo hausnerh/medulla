09ee524dec82860db0ef5462df99ba85ee1c5066
Harry Hausner <56450208+hausnerh@users.noreply.github.com>
2026-04-20 17:44:24 -0500

    moved precompute

--- spineplot/sample.py
+++ spineplot/sample.py
@@ -94,14 +94,15 @@ class Sample:
             self.override_exposure(override_exposure, exposure_type)
 
         self._data = pd.concat([self._file_handle[tree].arrays(library='pd') for tree in trees])
+
+        if precompute is not None:
+            for k, v in precompute.items():
+                self._data[k] = self._data.eval(v)
+
         if self._category_branch not in self._data.columns:
             raise ValueError(f'Category branch `{self._category_branch}` not found in sample `{self._name}`.')
         if override_category is not None:
             self._data[self._category_branch] = override_category
-        
-        if precompute is not None:
-            for k, v in precompute.items():
-                self._data[k] = self._data.eval(v)
         
         if presel is not None:
             self._presel_mask = self._data.eval(presel)
